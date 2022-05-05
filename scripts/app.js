a0b7bc630cd5d005c6cfd80fe08557da659cdf35
Mobin Atrzadeh <mbynatrzade@gmail.com>
2022-05-05 12:55:57 +0430

    add styles responsive header and adding js code

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+let icon = document.querySelector(".menu-icon");
+let menu = document.querySelector(".main-menu");
+
+icon.addEventListener("click", function () {
+    if (icon.classList.contains("fa-bars")) {
+        menu.style.left = 0;
+        icon.classList = "fa fa-times menu-icon";
+    } else {
+        menu.style.left = "-250px";
+        icon.classList = "fa fa-bars menu-icon";
+    }
+})
